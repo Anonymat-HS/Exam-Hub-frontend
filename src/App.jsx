8e5b8db81560386dd83b8b749ed77f7771a3d2e2
Herimihaja Steevy RASOAMIARAMANANA <steevyherimihaja@gmail.com>
2026-08-23 04:50:20 +0300

    feat(auth): mount centralized router in app root

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AdminLayout } from './layouts/AdminLayout';
-import { AdminDashboardPage } from './pages/admin/AdminDashboardPage';
-import { StudentsPage } from './pages/admin/StudentsPage';
-import { CoursesPage } from './pages/admin/CoursesPage';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './routes/AppRouter';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AdminLayout />}>
-          <Route path="/admin" element={<AdminDashboardPage />} />
-          <Route path="/admin/students" element={<StudentsPage />} />
-          <Route path="/admin/courses" element={<CoursesPage />} />
-        </Route>
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   );
 }
