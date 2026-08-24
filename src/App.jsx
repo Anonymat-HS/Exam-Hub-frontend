a8166c1dc3765288681bd49df698b4c92e81faf8
Randrianantenaina Julien <hei.julien.17@gmail.com>
2026-08-24 10:29:38 +0300

    refactor(app): remove unused imports and adjust router paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,} from 'react-router-dom';
 import { StudentLayout } from './layouts/StudentLayout.jsx';
 import { StudentDashboardPage } from './pages/student/StudentDashboardPage';
 import { ExamStudentPage } from './pages/student/ExamStudentPage.jsx';
@@ -12,7 +12,7 @@ export default function App() {
 
         <Route path="/student" element={<StudentLayout />}>
           <Route index element={<StudentDashboardPage />} />
-          <Route path="examens" element={<ExamStudentPage />} />
+          <Route path="exams" element={<ExamStudentPage />} />
           <Route path="results" element={<div>Page des résultats bientôt disponible</div>} />
         </Route>
 
