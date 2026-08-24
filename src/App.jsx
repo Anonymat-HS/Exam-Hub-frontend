07cb33b7173ce1d1e7f59b2587aba3e7b7b20a90
Randrianantenaina Julien <hei.julien.17@gmail.com>
2026-08-24 08:03:20 +0300

    refactor(route): update ExamStudentPage path in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { StudentLayout } from './layouts/StudentLayout.jsx';
 import { StudentDashboardPage } from './pages/student/StudentDashboardPage';
 import { ExamStudentPage } from './pages/student/ExamStudentPage.jsx';
+// import { LoginPage } from './pages/LoginPage.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+
         <Route path="/student" element={<StudentLayout />}>
-          
           <Route index element={<StudentDashboardPage />} />
-          
-          <Route path="exams" element={<ExamStudentPage />} />
+          <Route path="examens" element={<ExamStudentPage />} />
           <Route path="results" element={<div>Page des résultats bientôt disponible</div>} />
-          
-        </Route> 
+        </Route>
+
+        {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
       </Routes>
     </BrowserRouter>
   );
