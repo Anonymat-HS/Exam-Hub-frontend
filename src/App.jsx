dcd92fce2a76c6e441843aac2ac119c1b4634b3e
Randrianantenaina Julien <hei.julien.17@gmail.com>
2026-08-25 20:31:14 +0300

    feat: create exam result page an adjust path setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route,} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StudentLayout } from './layouts/StudentLayout.jsx';
 import { StudentDashboardPage } from './pages/student/StudentDashboardPage';
 import { ExamStudentPage } from './pages/student/ExamStudentPage.jsx';
+import { ExamResultPage } from './pages/student/ExamResultPage.jsx';
 // import { LoginPage } from './pages/LoginPage.jsx';
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
           <Route index element={<StudentDashboardPage />} />
           <Route path="exams" element={<ExamStudentPage />} />
           <Route path="results" element={<div>Page des résultats bientôt disponible</div>} />
+          <Route path="results/:examId" element={<ExamResultPage />} />
         </Route>
 
         {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
