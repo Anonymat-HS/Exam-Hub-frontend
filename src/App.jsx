5dee7382d04ade7773def33e0567f739dddc247a
Randrianantenaina Julien <hei.julien.17@gmail.com>
2026-08-25 06:16:54 +0300

    feat(student): add take exam page and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+<<<<<<< Updated upstream
 export default function App() {
   return (
     <main style={{ fontFamily: 'system-ui, sans-serif', textAlign: 'center', marginTop: '4rem' }}>
       <h1>Exam Hub</h1>
       <p>Squelette frontend — à implémenter.</p>
     </main>
+=======
+import { BrowserRouter, Routes, Route,} from 'react-router-dom';
+import { StudentLayout } from './layouts/StudentLayout.jsx';
+import { StudentDashboardPage } from './pages/student/StudentDashboardPage';
+import { ExamStudentPage } from './pages/student/ExamStudentPage.jsx';
+import { TakeExamPage } from './pages/student/TakeExamPage.jsx';
+// import { LoginPage } from './pages/LoginPage.jsx';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+
+        <Route path="/student" element={<StudentLayout />}>
+          <Route index element={<StudentDashboardPage />} />
+          <Route path="exams" element={<ExamStudentPage />} />
+          <Route path="results" element={<div>Page des résultats bientôt disponible</div>} />
+          <Route path="exams/:examId" element={<TakeExamPage />} />
+        </Route>
+
+        {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
+      </Routes>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   );
 }
