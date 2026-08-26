46f8fa9a785e046b828b283916e06af98f1b1099
Randrianantenaina Julien <hei.julien.17@gmail.com>
2026-08-26 20:29:35 +0300

    feat(routing): unify application routes across all student modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { BrowserRouter, Routes, Route,} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage.jsx';
 import { StudentLayout } from './layouts/StudentLayout.jsx';
-import { StudentDashboardPage } from './pages/student/StudentDashboardPage';
+import { StudentDashboardPage } from './pages/student/StudentDashboardPage.jsx';
 import { ExamStudentPage } from './pages/student/ExamStudentPage.jsx';
-// import { LoginPage } from './pages/LoginPage.jsx';
+import { TakeExamPage } from './pages/student/TakeExamPage.jsx';
+import { StudentResultsPage } from './pages/student/StudentResultsPage.jsx';
+import { ExamResultPage } from './pages/student/ExamResultPage.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/login" element={<LoginPage />} />
 
         <Route path="/student" element={<StudentLayout />}>
           <Route index element={<StudentDashboardPage />} />
           <Route path="exams" element={<ExamStudentPage />} />
-          <Route path="results" element={<div>Page des résultats bientôt disponible</div>} />
+          <Route path="exams/:examId" element={<TakeExamPage />} />
+          <Route path="results" element={<StudentResultsPage />} />
+          <Route path="results/:examId" element={<ExamResultPage />} />
         </Route>
 
-        {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
