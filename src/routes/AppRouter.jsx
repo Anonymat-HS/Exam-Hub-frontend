3f85531a70f4e7a36c05522a35687522f7ae1161
Randrianantenaina Julien <hei.julien.17@gmail.com>
2026-08-27 06:06:14 +0300

    feat: create AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,1 +1,31 @@
-TO DO
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+import { StudentLayout } from '../layouts/StudentLayout';
+
+import { StudentDashboardPage } from '../pages/student/StudentDashboardPage';
+import { ExamStudentPage } from '../pages/student/ExamStudentPage';
+import { TakeExamPage } from '../pages/student/TakeExamPage';
+import { StudentResultsPage } from '../pages/student/StudentResultsPage';
+import { ExamResultPage } from '../pages/student/ExamResultPage';
+
+export function AppRouter() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/student" replace />} />
+
+          <Route path="/student" element={<StudentLayout />}>
+            <Route index element={<StudentDashboardPage />} />
+            <Route path="exams" element={<ExamStudentPage />} />
+            <Route path="exams/:examId" element={<TakeExamPage />} />
+            <Route path="results" element={<StudentResultsPage />} />
+            <Route path="results/:examId" element={<ExamResultPage />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/student" replace />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
