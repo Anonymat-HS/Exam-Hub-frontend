ac9729e5a0261d9d4a322fec402836240116d4d3
Herimihaja Steevy RASOAMIARAMANANA <steevyherimihaja@gmail.com>
2026-08-28 11:06:07 +0300

    fix(layout): center admin content with max-w-5xl mx-auto matching student layout

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -126,7 +126,7 @@ export function AdminLayout() {
 
       {/* Main content */}
       <main className="flex-1 overflow-y-auto pt-14 lg:pt-0">
-        <div className="p-4 sm:p-6 lg:p-8">
+        <div className="mx-auto max-w-5xl p-4 sm:p-6 lg:p-8">
           <Outlet />
         </div>
       </main>
